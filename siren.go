60ae013b9112c47086c61319588fc09a47447f77
bcmk <45658475+bcmk@users.noreply.github.com>
2019-02-06 06:08:26 +0400

    cosmetic

--- siren.go
+++ siren.go
@@ -34,10 +34,10 @@ func Lerr(format string, v ...interface{}) { log.Printf("[ERROR] "+format, v...)
 // Linf logs an info message
 func Linf(format string, v ...interface{}) { log.Printf("[INFO] "+format, v...) }
 
-// Ldbg logs debug message
+// Ldbg logs a debug message
 func Ldbg(format string, v ...interface{}) { log.Printf("[DEBUG] "+format, v...) }
 
-// CheckErr panics on error
+// CheckErr panics on an error
 func CheckErr(err error) {
 	if err != nil {
 		panic(err)
