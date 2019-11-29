9f8e630b8aab505ded297719d5b29b71e03713d6
bcmk <45658475+bcmk@users.noreply.github.com>
2019-11-29 05:49:14 +0400

    stripchat, adding default headers to tool

--- cmd/stripchat/main.go
+++ cmd/stripchat/main.go
@@ -13,6 +13,16 @@ var timeout = flag.Int("t", 10, "timeout in seconds")
 var address = flag.String("a", "", "source IP address")
 var cookies = flag.Bool("c", false, "use cookies")
 
+var headers = [][2]string{
+	{"authority", "stripchat.com"},
+	{"dnt", "1"},
+	{"upgrade-insecure-requests", "1"},
+	{"user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"},
+	{"accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"},
+	{"sec-fetch-site", "none"},
+	{"sec-fetch-mode", "navigate"},
+	{"accept-language", "en-US,en;q=0.9"}}
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: %s [options] <model ID>\n", os.Args[0])
@@ -29,5 +39,5 @@ func main() {
 		return
 	}
 	client := lib.HTTPClientWithTimeoutAndAddress(*timeout, *address, *cookies)
-	fmt.Println(lib.CheckModelStripchat(client, modelID, nil, *verbose))
+	fmt.Println(lib.CheckModelStripchat(client, modelID, headers, *verbose))
 }
