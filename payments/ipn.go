403d19d631c09464376ba4eae2e5030e91173fc9
bcmk <45658475+bcmk@users.noreply.github.com>
2020-04-24 14:32:55 +0400

    closing body of IPN request

--- payments/ipn.go
+++ payments/ipn.go
@@ -18,6 +18,7 @@ func ParseIPN(r *http.Request, ipnSecret string, debug bool) (StatusKind, string
 	if err != nil {
 		return StatusUnknown, "", errors.New("cannot read body")
 	}
+	lib.CheckErr(r.Body.Close())
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 	if debug {
