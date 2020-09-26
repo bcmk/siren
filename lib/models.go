01265c695824ccb6fd9b82a10b9a7a8292fbda00
bcmk <siren.telegram@gmail.com>
2020-09-26 17:20:17 +0400

    support for @xh models

--- lib/models.go
+++ lib/models.go
@@ -3,7 +3,7 @@ package lib
 import "regexp"
 
 // ModelIDRegexp is a regular expression to check model IDs
-var ModelIDRegexp = regexp.MustCompile(`^[a-z0-9\-_]+$`)
+var ModelIDRegexp = regexp.MustCompile(`^[a-z0-9\-_@]+$`)
 
 // StatusRequest represents a request of model status
 type StatusRequest struct {
