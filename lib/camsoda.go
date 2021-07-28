0777938a6bfba4ce5cc30300e96b5907db8aa88b
bcmk <siren.chat@gmail.com>
2021-07-28 23:01:17 +0400

    removing unsupported API

--- lib/camsoda.go
+++ lib/camsoda.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"strings"
 )
 
@@ -36,64 +35,6 @@ type camSodaOnlineResponse struct {
 
 // CheckStatusSingle checks CamSoda model status
 func (c *CamSodaChecker) CheckStatusSingle(modelID string) StatusKind {
-	client := c.clientsLoop.nextClient()
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://feed.camsoda.com/api/v1/user/%s", modelID), nil)
-	CheckErr(err)
-	for _, h := range c.Headers {
-		req.Header.Set(h[0], h[1])
-	}
-	resp, err := client.Client.Do(req)
-	if err != nil {
-		Lerr("[%v] cannot send a query, %v", client.Addr, err)
-		return StatusUnknown
-	}
-	defer func() {
-		CheckErr(resp.Body.Close())
-	}()
-	if c.Dbg {
-		Ldbg("[%v] query status for %s: %d", client.Addr, modelID, resp.StatusCode)
-	}
-	if resp.StatusCode == 401 {
-		return StatusDenied
-	}
-	if resp.StatusCode == 404 {
-		return StatusNotFound
-	}
-	buf := bytes.Buffer{}
-	_, err = buf.ReadFrom(resp.Body)
-	if err != nil {
-		Lerr("[%v] cannot read response for model %s, %v", client.Addr, modelID, err)
-		return StatusUnknown
-	}
-	decoder := json.NewDecoder(ioutil.NopCloser(bytes.NewReader(buf.Bytes())))
-	parsed := &camSodaUserResponse{}
-	err = decoder.Decode(parsed)
-	if err != nil {
-		Lerr("[%v] cannot parse response for model %s, %v", client.Addr, modelID, err)
-		if c.Dbg {
-			Ldbg("response: %s", buf.String())
-		}
-		return StatusUnknown
-	}
-	if !parsed.Status {
-		Lerr("[%v] API error for model %s, %s", client.Addr, modelID, parsed.Error)
-		return StatusUnknown
-	}
-	return camSodaStatus(parsed.User.Chat.Status)
-}
-
-func camSodaStatus(roomStatus string) StatusKind {
-	switch roomStatus {
-	case "online":
-		return StatusOnline
-	case "limited":
-		return StatusOnline
-	case "private":
-		return StatusOnline
-	case "offline":
-		return StatusOffline
-	}
-	Lerr("cannot parse room status \"%s\"", roomStatus)
 	return StatusUnknown
 }
 
