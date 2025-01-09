8f1f01983508d06dd392b4c4ccfcb0d97fe91ce2
bcmk <siren.chat@gmail.com>
2025-01-09 02:14:56 +0000

    merge migrations

--- cmd/bot/migrations.go
+++ cmd/bot/migrations.go
@@ -7,84 +7,20 @@ import (
 var migrations = []func(w *worker){
 	func(w *worker) {
 		w.mustExec(`
-			create table if not exists signals (
-				chat_id integer,
-				model_id text,
-				endpoint text not null default '',
-				primary key (chat_id, model_id, endpoint));`)
+			create table block (
+				chat_id integer not null,
+				endpoint text not null,
+				block integer not null,
+				primary key (chat_id, endpoint)
+			);
+		`)
 		w.mustExec(`
-			create table if not exists status_changes (
-				model_id text,
-				status integer not null default 0,
-				timestamp integer not null default 0);`)
-		w.mustExec(`
-			create table if not exists last_status_changes (
-				model_id text primary key,
-				status integer not null default 0,
-				timestamp integer not null default 0);`)
-		w.mustExec(`
-			create table if not exists models (
-				model_id text primary key,
-				status integer not null default 0,
-				referred_users integer not null default 0);`)
-		w.mustExec(`
-			create table if not exists feedback (
+			create table feedback (
 				chat_id integer,
 				text text,
-				endpoint text not null default '');`)
-		w.mustExec(`
-			create table if not exists block (
-				chat_id integer,
-				block integer not null default 0,
-				endpoint text not null default '',
-				primary key(chat_id, endpoint));`)
-		w.mustExec(`
-			create table if not exists users (
-				chat_id integer primary key,
-				max_models integer not null default 0,
-				reports integer not null default 0);`)
-		w.mustExec(`
-			create table if not exists emails (
-				chat_id integer,
-				endpoint text not null default '',
-				email text not null default '',
-				primary key(chat_id, endpoint));`)
-		w.mustExec(`
-			create table if not exists transactions (
-				local_id text primary key,
-				kind text,
-				chat_id integer,
-				remote_id text,
-				timeout integer,
-				amount text,
-				address text,
-				status_url text,
-				checkout_url text,
-				dest_tag text,
-				status integer,
-				timestamp integer,
-				model_number integer,
-				currency text,
-				endpoint text not null default '');`)
-		w.mustExec(`
-			create table if not exists referrals (
-				chat_id integer primary key,
-				referral_id text not null default '',
-				referred_users integer not null default 0);`)
-	},
-	func(w *worker) {
-		w.mustExec("create index ix_status_changes_model_id on status_changes(model_id);")
-	},
-	func(w *worker) {
-		w.mustExec("alter table users add column blacklist integer not null default 0;")
-	},
-	func(w *worker) {
-		w.mustExec("alter table users add column show_images integer not null default 1;")
-	},
-	func(w *worker) {
-		w.mustExec("alter table users add column offline_notifications integer not null default 1;")
-	},
-	func(w *worker) {
+				endpoint text not null default ''
+			);
+		`)
 		w.mustExec(`
 			create table interactions (
 				priority integer not null,
@@ -92,24 +28,27 @@ var migrations = []func(w *worker){
 				endpoint text not null,
 				chat_id integer not null,
 				result integer not null,
-				delay integer not null);`)
-	},
-	func(w *worker) {
-		w.mustExec("alter table models add column special integer not null default 0;")
-	},
-	func(w *worker) {
-		w.mustExec("create index ix_status_changes_timestamp on status_changes(timestamp);")
-	},
-	func(w *worker) {
-		w.mustExec("alter table signals add column confirmed integer not null default 1;")
-		w.mustExec("create index ix_signals_confirmed on signals(confirmed);")
-	},
-	func(w *worker) {
-		w.mustExec("update models set status = 1 << (status - 1);")
-		w.mustExec("update status_changes set status = 1 << (status - 1);")
-		w.mustExec("update last_status_changes set status = 1 << (status - 1);")
-	},
-	func(w *worker) {
+				delay integer not null,
+				kind integer not null default 0
+			);
+		`)
+		w.mustExec(`create index ix_interactions_endpoint on interactions (endpoint);`)
+		w.mustExec(`create index ix_interactions_timestamp on interactions ("timestamp");`)
+		w.mustExec(`
+			create table last_status_changes (
+				model_id text primary key,
+				status integer not null default 0,
+				timestamp integer not null default 0
+			);
+		`)
+		w.mustExec(`
+			create table models (
+				model_id text primary key,
+				status integer not null default 0,
+				referred_users integer not null default 0,
+				special integer not null default 0
+			);
+		`)
 		w.mustExec(`
 			create table notification_queue (
 				id serial primary key,
@@ -122,19 +61,45 @@ var migrations = []func(w *worker){
 				social boolean not null default false,
 				priority integer not null default 0,
 				sound boolean not null default false,
-				sending integer not null default 0);`)
-	},
-	func(w *worker) {
-		w.mustExec("create index ix_interactions_endpoint on interactions(endpoint);")
-		w.mustExec("create index ix_interactions_timestamp on interactions(timestamp);")
-	},
-	func(w *worker) {
-		w.mustExec("alter table interactions add column kind integer not null default 0;")
-		w.mustExec("alter table notification_queue add column kind integer not null default 0;")
-	},
-	func(w *worker) {
-		w.mustExec("drop table transactions;")
-		w.mustExec("drop table emails;")
+				sending integer not null default 0,
+				kind integer not null default 0
+			);
+		`)
+		w.mustExec(`
+			create table referrals (
+				chat_id integer primary key,
+				referral_id text not null default '',
+				referred_users integer not null default 0
+			);
+		`)
+		w.mustExec(`
+			create table signals (
+				chat_id integer not null,
+				model_id text not null,
+				endpoint text not null default '',
+				confirmed integer not null default 1,
+				primary key (chat_id, model_id, endpoint)
+			);
+		`)
+		w.mustExec(`create index ix_signals_confirmed on signals (confirmed);`)
+		w.mustExec(`
+			create table status_changes (
+				model_id text,
+				status integer not null default 0,
+				timestamp integer not null default 0
+			);`)
+		w.mustExec(`create index ix_status_changes_model_id on status_changes (model_id);`)
+		w.mustExec(`create index ix_status_changes_timestamp on status_changes ("timestamp");`)
+		w.mustExec(`
+			create table users (
+				chat_id integer primary key,
+				max_models integer not null default 0,
+				reports integer not null default 0,
+				blacklist integer not null default 0,
+				show_images integer not null default 1,
+				offline_notifications integer not null default 1
+			);
+		`)
 	},
 }
 
