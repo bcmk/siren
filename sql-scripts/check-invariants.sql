11a474074caf9b7e50eb945cc2d8daecc3c14fbf
bcmk <siren.chat@gmail.com>
2026-01-08 11:56:21 +0000

    fix: update check-invariants.sql script to latest DB model

--- sql-scripts/check-invariants.sql
+++ sql-scripts/check-invariants.sql
@@ -3,15 +3,12 @@ with periods as (
         model_id,
         status,
         timestamp,
-        is_latest,
 
         lead(timestamp) over (partition by model_id order by timestamp) as next_timestamp,
         lead(status)    over (partition by model_id order by timestamp) as next_status,
-        lead(is_latest) over (partition by model_id order by timestamp) as next_is_latest,
 
         lag(timestamp)  over (partition by model_id order by timestamp) as prev_timestamp,
-        lag(status)     over (partition by model_id order by timestamp) as prev_status,
-        lag(is_latest)  over (partition by model_id order by timestamp) as prev_is_latest
+        lag(status)     over (partition by model_id order by timestamp) as prev_status
     from status_changes
 )
 select *
